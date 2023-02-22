0ac2975c2fa3716fcaee9934c795c5c2599981c3
ssemugenyiisaac2@gmail.com <ssemugenyiisaac2@gmail.com>
2023-02-23 02:07:21 +0300

    Added change title button

--- expense-tracker/src/components/Expenses/ExpenseItem.js
+++ expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -4,12 +4,19 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  /**
+   * Click handler - Handles clicks on change title button
+   */
+  const clickHandler = () => {
+    console.log("Clicked");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Change title</button>
       </div>
     </Card>
   );
