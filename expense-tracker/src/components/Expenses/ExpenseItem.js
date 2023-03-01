a6165c7fb3fec0dc1e89da0faf65e8b2d7ec3b1e
ssemugenyiisaac2@gmail.com <ssemugenyiisaac2@gmail.com>
2023-03-02 01:55:36 +0300

    (feat):Added a usestate hook

--- expense-tracker/src/components/Expenses/ExpenseItem.js
+++ expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
   /**
    * Click handler - Handles clicks on change title button
    */
   const clickHandler = () => {
-    console.log("Clicked");
+    setTitle("Updated");
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
         <button onClick={clickHandler}>Change title</button>
       </div>
